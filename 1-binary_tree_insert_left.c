369d4c5cf42abe7abde67debbf3eda65857c3e73
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-08-31 12:36:25 -0700

    1-binary_tree_insert_left: return if malloc fails

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,6 +13,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 	new_child = (binary_tree_t *)malloc(sizeof(*new_child));
+	if (new_child == NULL)
+		return (NULL);
 	new_child->parent = parent;
 	new_child->n = value;
 	new_child->right = NULL;
